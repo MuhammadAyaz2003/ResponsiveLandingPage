9cb7b9e5f0b45c0a2e969b97a009267b98c08d64
MuhammadAyaz2003 <muhammadayazayaz2003@gmail.com>
2023-11-01 09:56:36 +0500

    gsap

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // Select the navigation and content elements
 const nav = document.querySelector('.nav');
 const content = document.querySelector('.content');
@@ -7,9 +5,20 @@ const content = document.querySelector('.content');
 // Create a timeline for animating the elements
 const pageTimeline = gsap.timeline();
 
-// Add animations to the timeline
-pageTimeline.from(nav, { opacity: 0, y: -50, duration: 1, ease: 'power2.out' })
-            .from(content, { opacity: 0, x: -50, duration: 1, ease: 'power2.out' }, '-=0.5'); // Start content animation 0.5 seconds before nav ends
+// Add staggered animations with custom easing and durations
+pageTimeline.from(nav, {
+  opacity: 0,
+  y: -50,
+  duration: .6,
+  ease: 'ease-in-out',
+})
+.from(content, {
+  opacity: 0,
+  x: -50,
+  duration: .6,
+  ease: 'ease-in-out',
+  delay: 0.3,
+});
 
 // Play the animations when the page is ready
 window.addEventListener('load', function () {
